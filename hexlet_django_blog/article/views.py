ef71a6002c9395267e88880db36e76f552f92a83
Максим Шевцов <shevtsov_stv@mail.ru>
2025-07-30 08:48:37 +0300

    Update articles

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -2,13 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.views import View
 
 
-def index(request):
-    return render(
-        request,
-        "articles/indexx.html",
-        context={
-            "name": "Article",
-        },
-    )
+class IndexView(View):
+
+
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("My app name is Hexlet-blog")
+        
