32a390deea1d35ace958708866bf5596e9ebb0f3
Максим Шевцов <shevtsov_stv@mail.ru>
2025-08-04 16:14:29 +0300

    Create superuser

--- hexlet_django_blog/article/admin.py
+++ hexlet_django_blog/article/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Article
+from django.contrib.admin import DateFieldListFilter
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "created_at",
+    )  # Перечисляем поля, отображаемые в таблице списка статей
+    search_fields = ["name", "body"]
+    list_filter = (
+        ("created_at", DateFieldListFilter),
+    )
+
 
 # Register your models here.
